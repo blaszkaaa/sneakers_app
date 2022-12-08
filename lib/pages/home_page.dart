42abf8d02439fb4d1959e03a062ee20032ccf31f
blaszkaaa <58654924+blaszkaaa@users.noreply.github.com>
2022-12-08 19:02:05 +0100

    1

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,8 +11,54 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
-        children: [],
+        children: [
+          Image.asset(
+            "images/but.png",
+            scale: 1.1,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 24),
+            child: Column(
+              children: [
+                Row(
+                  children: const [
+                    Text(
+                      "Just do it with",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    Text(
+                      " Nike",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: const [
+                    Text(
+                      "Get access to more than 1000 nike shoes\n also another brands with %20 off",
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
